51ffd3ac9ab74e5945c34b3f0b082e81f5e6c14c
Arthur Stace <arthur.stace@pm.me>
2021-01-23 08:41:33 +1100

    main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
-module "consul" {
+module "admin_ui" {
+  source = "github.com/arthur-stace/radical"
+}
+module "public_ui" {
+  source = "github.com/arthur-stace/backpack"
+}
+module "database" {
   source = "github.com/arthur-stace/tape"
 }
